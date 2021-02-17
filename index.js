c15d0262baf44066af948e43520a3afcd2126503
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-02-17 16:32:18 -0500

    adds 31 flavors solution

--- index.js
+++ index.js
@@ -58,14 +58,18 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
     1. an array as a parameter
     2. Check to see if the array given is 31 flavors
     3. Your function should return a boolean TRUE if the length of the array is 31 and FALSE if the length of the array is NOT 31.
-  
+
 
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array){
+    if (array.length === 31) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
